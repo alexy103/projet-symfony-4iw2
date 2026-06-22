d291fdf49f9299b70abeb8aea73591871ac6f2b2
Escanor-dev95 <floflobarroso@gmail.com>
2026-06-22 11:01:42 +0200

    feat(weather): show excuse-passability weather on the dashboard

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\WeatherService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(): Response
+    public function index(WeatherService $weatherService): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'weather' => $weatherService->getExcuseWeather(),
+        ]);
     }
 }
